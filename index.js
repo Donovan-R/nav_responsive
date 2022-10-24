dca4ad5b6bf307b9af8c6462ddfcb9ea39857c73
donovan <donoriviere@gmail.com>
2022-10-24 14:29:49 +0200

    init_nav

---
+++ index.js
@@ -0,0 +1,12 @@
+const toggle = document.querySelector(".toggle-btn");
+const links = document.querySelector(".links");
+
+toggle.addEventListener("click", function () {
+  //   if (links.classList.contains("show-links")) {
+  //     links.classList.remove("show-links");
+  //   } else {
+  //     links.classList.add("show-links");
+  // }
+
+  links.classList.toggle("show-links");
+});
